2a323f4d904d5a506f17ddb7279271fbd85a631b
Pedro Alquimim <89994181+pedrOAlquimim@users.noreply.github.com>
2022-06-13 19:58:34 -0300

    Using HeadlessUI for accessibility

---
+++ webWidget/src/Components/Widget.tsx
@@ -0,0 +1,22 @@
+import { ChatTeardropDots } from 'phosphor-react'
+import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm'
+
+export function Widget() {
+  return (
+    <Popover className="absolute right-4 bottom-4 md:bottom-8 md:right-8 flex flex-col items-end">
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
+
+      <Popover.Button className="bg-violet-500 rounded-full px-3 h-12 flex items-center text-white group">
+        <ChatTeardropDots className="w-6 h-6" />
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
+  )
+}
