dda7a8bdce485330f725de4e6c7b3d7a983701f6
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-07-26 15:03:06 -0300

    Creator name changed

--- webWidget/src/Components/WidgetForm/index.tsx
+++ webWidget/src/Components/WidgetForm/index.tsx
@@ -63,7 +63,7 @@ export function WidgetForm() {
       )}
 
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela <a className="underline underline-offset-2" href="https://www.rocketseat.com.br/">Rocketseat</a>
+        Feito com ♥ por <a className="underline underline-offset-2" href="https://www.rocketseat.com.br/">Pedro</a>
       </footer>
     </div>
   )
